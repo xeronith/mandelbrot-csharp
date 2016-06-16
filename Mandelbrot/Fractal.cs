6eccea6542ccc9769255ea63674685b1cff52067
Xeronith <xeronith@yahoo.com>
2016-06-17 02:26:42 +0430

    Add fractal object.

---
+++ Mandelbrot/Fractal.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandelbrot
+{
+    public delegate void ImageHandler(Image fractal);
+    public static class Fractal
+    {
+        public static event ImageHandler ImageGenerated;
+        public static unsafe void Generate(int centerX, int centerY, int size, double range)
+        {
+            var image = new Bitmap(size, size, PixelFormat.Format24bppRgb);
+            var bitmapData = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.WriteOnly, image.PixelFormat);
+
+            byte* scan0 = (byte*)bitmapData.Scan0.ToPointer();
+            int bytesPerPixel = 3, stride = bitmapData.Stride;
+
+            int width = bitmapData.Width, height = bitmapData.Height, maxIterations = 32;
+
+            for (int row = 0; row < height; row++)
+            {
+                byte* pixel = scan0 + (row * stride);
+
+                for (int column = 0; column < width; column++)
+                {
+                    var cReal = (column - centerX) * range / width;
+                    var cImaginary = (row - centerY) * range / height;
+
+                    double x = 0, y = 0;
+
+                    int iterations = 0;
+
+                    while (x * x + y * y < 4 && iterations < maxIterations)
+                    {
+                        var _x = x * x - y * y + cReal;
+                        y = 2 * x * y - cImaginary;
+                        x = _x;
+
+                        iterations++;
+                    }
+
+                    int offset = column * bytesPerPixel;
+
+                    if (iterations < maxIterations)
+                    {
+                        double norm = iterations / (double)maxIterations;
+                        int max = 255;
+
+                        //R
+                        pixel[offset + 2] = (byte)(norm * max / 1);
+                        //G
+                        pixel[offset + 1] = (byte)(norm * max / 2);
+                        //B
+                        pixel[offset + 0] = (byte)(norm * max / 4);
+                    }
+                    else
+                    {
+                        pixel[offset + 2] = 0;
+                        pixel[offset + 1] = 0;
+                        pixel[offset + 0] = 0;
+                    }
+                }
+            }
+
+            image.UnlockBits(bitmapData);
+
+            ImageGenerated?.Invoke(image);
+        }
+    }
+}
--- Mandelbrot/Program.cs
+++ Mandelbrot/Program.cs
@@ -11,7 +11,6 @@ namespace Mandelbrot
     class Program : Form
     {
         private PictureBox pictureBox;
-        private int size = 500;
         public Program()
         {
             Text = "Mandelbrot Set";
@@ -21,11 +20,7 @@ namespace Mandelbrot
 
             pictureBox = new PictureBox();
             Controls.Add(pictureBox);
-
-            SetImage(size / 2, size / 2);
-
             pictureBox.MouseDown += delegate {
-
                 var saveFileDialog = new SaveFileDialog();
                 saveFileDialog.Filter = "PNG Files|*.png";
                 saveFileDialog.FileName = "Fractal";
@@ -33,73 +28,13 @@ namespace Mandelbrot
                 if(saveFileDialog.ShowDialog() == DialogResult.OK)
                     pictureBox.Image.Save(saveFileDialog.FileName, ImageFormat.Png);
             };
-        }
 
-        private void SetImage(int x, int y)
-        {
-            var image = GenerateFractal(x, y, size, 4.0);
-            ClientSize = pictureBox.Size = image.Size;
-            pictureBox.Image = image;
-        }
+            Fractal.ImageGenerated += fractal => {
+                ClientSize = pictureBox.Size = fractal.Size;
+                pictureBox.Image = fractal;
+            };
 
-        private unsafe Bitmap GenerateFractal(int centerX, int centerY, int size, double range)
-        {
-            var image = new Bitmap(size, size, PixelFormat.Format24bppRgb);
-            var bitmapData = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.WriteOnly, image.PixelFormat);
-
-            byte* scan0 = (byte*)bitmapData.Scan0.ToPointer();
-            int bytesPerPixel = 3, stride = bitmapData.Stride;
-
-            int width = bitmapData.Width, height = bitmapData.Height, maxIterations = 32;
-
-            for (int row = 0; row < height; row++)
-            {
-                byte* pixel = scan0 + (row * stride);
-
-                for (int column = 0; column < width; column++)
-                {
-                    var cReal = (column - centerX) * range / width;
-                    var cImaginary = (row - centerY) * range / height;
-
-                    double x = 0, y = 0;
-
-                    int iterations = 0;
-
-                    while(x * x + y * y < 4 && iterations < maxIterations)
-                    {
-                        var _x = x * x - y * y + cReal;
-                        y = 2 * x * y - cImaginary;
-                        x = _x;
-
-                        iterations++;
-                    }
-
-                    int offset = column * bytesPerPixel;
-
-                    if (iterations < maxIterations)
-                    {
-                        double norm = iterations / (double)maxIterations;
-                        int max = 255;
-
-                        //R
-                        pixel[offset + 2] = (byte)(norm * max / 1);
-                        //G
-                        pixel[offset + 1] = (byte)(norm * max / 2);
-                        //B
-                        pixel[offset + 0] = (byte)(norm * max / 4);
-                    }
-                    else
-                    {
-                        pixel[offset + 2] = 0;
-                        pixel[offset + 1] = 0;
-                        pixel[offset + 0] = 0;
-                    }
-                }
-            }
-
-            image.UnlockBits(bitmapData);
-
-            return image;
+            Fractal.Generate(250, 250, 500, 4.0);
         }
 
         [STAThread]
